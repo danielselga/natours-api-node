34ca884968b6067d9bb21eb71e3b77b8e02ff777
Daniel Selga <danielselga@gmail.com>
2022-01-30 19:23:13 -0300

    chore: node native error middleware

--- app.js
+++ app.js
@@ -33,15 +33,25 @@ app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRouter);
 
 // All the http methods (works because the last middleware check the routes and execute.)
-app.all('*', async (req, res, next) => {
-  try {
-    res.status(404).json({
-      status: 'fail',
-      message: `Can't find ${req.originalUrl}`
-    })
-  } catch (err) {
-    console.log(err)
-  }
+app.all('*', (req, res, next) => {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: `Can't find ${req.originalUrl}`
+  //   })
+  const err = new Error(`Can't find ${req.originalUrl} on this server`)
+  err.status = 'fail'
+  err.statusCode = 404
+
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error'
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
 })
 
 module.exports = app
