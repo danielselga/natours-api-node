1e2f4c10421807955ddec70b1bca6f49280a33f7
Daniel Selga <danielselga@gmail.com>
2022-01-31 19:35:55 -0300

    chore: appError and errorMIddleware import

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const morgan = require('morgan');
 const userRouter = require('./routes/userRoutes');
 const tourRoutes = require('./routes/tourRoutes');
 const app = express();
+const appError = require('./utils/appError');
+const errorMidleware = require('./middlewares/errorMiddleware')
 
 // Morgan only on with enviroment variable
 if(process.env.NODE_ENV === 'development') {
   console.log('Morgan on')
   app.use(morgan('dev'));
 }
+
 
 // Middlewares to Parsing the requisitions body
 app.use(express.json()); // Extreme important to post and patch methods
@@ -34,25 +37,16 @@ app.use('/api/v1/users', userRouter);
 
 // All the http methods (works because the last middleware check the routes and execute.)
 app.all('*', (req, res, next) => {
-  console.log(err.stack)
   //   res.status(404).json({
   //     status: 'fail',
   //     message: `Can't find ${req.originalUrl}`
   //   })
-  const err = new Error(`Can't find ${req.originalUrl} on this server`)
-  err.status = 'fail'
-  err.statusCode = 404
-
-  next(err)
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`)
+  // err.status = 'fail'
+  // err.statusCode = 404
+  next(new appError(`Can't find ${req.originalUrl} on this server`, 404))
 })
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(errorMidleware.globalErrorHandler)
 
 module.exports = app
