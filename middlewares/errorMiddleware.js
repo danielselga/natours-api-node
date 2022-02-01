a4e317a63329f7ead70122f50ad05809b380d45f
Daniel Selga <danielselga@gmail.com>
2022-02-01 00:39:48 -0300

    chore: handling duplicate fields and cast err

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,12 +1,24 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`
+  return AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+  const message = `Duplicate field value: ${value}. Please use another value!`
+  return new AppError(message, 400)
+}
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
-    stack: err.stack
+    stack: err.stack,
   });
-}
+};
 
 const SendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client.
@@ -18,24 +30,30 @@ const SendErrorProd = (err, res) => {
     // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log Error
-    console.log('Error', err)
+    console.log('Error', err);
 
     // 2) Send the generic message
     res.status(500).json({
       status: 'error',
-      messege: 'Something went very wrong!'
-    })
+      messege: 'Something went very wrong!',
+    });
   }
-}
+};
 
 exports.globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res)
+    sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    SendErrorProd(err, res)
+    let error = {...err}
+    if(error.name === 'CastError') {
+      return error = handleCastErrDB(error)
+    }
+    if (error.code === 11000) {
+      return error = handleDuplicateFieldsDB(error)
+    }
+    SendErrorProd(error, res);
   }
-
 };
