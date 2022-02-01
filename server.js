9190ccec2a3038bb4150c6934a49e631582d2d8e
Daniel Selga <danielselga@gmail.com>
2022-02-01 18:16:03 -0300

    chore: handling process error

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotEnv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXEPTION!')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
+
 dotEnv.config({
   path: './config.env',
 });
@@ -12,23 +19,26 @@ const DB = process.env.DB_URI.replace(
   process.env.DATABASE_PASSWORD
 );
 
-try {
-  mongoose
-    .connect(DB, {
-      useNewUrlParser: true,
-    })
-    .then((con) => {
-      console.log('DB Connection Successfull');
-    });
-} catch (err) {
-  console.err(err);
-}
+mongoose
+  .connect(DB, { useNewUrlParser: true })
+  .then((con) => {
+    console.log('DB Connection Successfull');
+  })
+  
 
-mongoose.connection.on('error', (err) => {
-  logError(err);
-});
+// mongoose.connection.on('error', (err) => {
+//   console.log(err);
+// });
 
 // Opening the server
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`App runing on port ${process.env.PORT}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION!')
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1)
+  })
+})
