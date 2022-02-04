f2c4d8ad73dcc57ece4597bbf8935b56ce9e60f5
Daniel Selga <danielselga@gmail.com>
2022-02-04 02:34:49 -0300

    chore: timestamp of creation accout and token creation check

--- middlewares/protectMiddleware.js
+++ middlewares/protectMiddleware.js
@@ -25,13 +25,19 @@ exports.protect = catchAsync(async (req, res, next) => {
   // 2) Verify the token
 
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
-  console.log(decoded)
   
   // 3) Check if user still exists
   
   const freshUser = await User.findById(decoded.id)
+
+  if (!freshUser) {
+    return next(new AppError('The user belonging to this token does no longer exist!', 401))
+  }
   
   // 4) Check if user changed password after the jwt was issued
+  if (freshUser.changePasswordAfter(decoded.iat)) {
+    return next(new AppError('User recently change password, Please login again!'))
+  }
 
   next();
 }); 
