34a1ffb0ad0144e2daf7752c718b4c5ee62ca47d
Daniel Selga <danielselga@gmail.com>
2022-02-09 00:55:32 -0300

    chore: forgot and reset password feature finished

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -95,12 +96,30 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
 });
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = catchAsync( async(req, res, next) => {
   // 1) Get user based on the token
+  const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+  const user = await User.findOne({passwordResetToken: hashedToken, passwordResetExpires: {$gt: Date.now()}})
 
   // 2) If token has not expired, and there is user, set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or expired', 400))
+  }
+
+  user.password = req.body.password
+  user.passwordConfirm = req.body.passwordConfirm
+  user.passwordResetExpires = undefined
+  user.passwordResetExpires = undefined
+  await user.save()
 
   // 3) Update ChangedPasswordAt property for the user
+  const token = signToken(user._id)
+
+  res.status(200).json({
+    status: 'success',
+    token
+  })
 
   // 4) Log the user in, send JWT
-}
+})
