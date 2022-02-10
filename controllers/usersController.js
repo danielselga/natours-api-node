d7872f8c3c5decfaaa2ad97e5df3450ce59ebc7f
Daniel Selga <danielselga@gmail.com>
2022-02-09 21:41:04 -0300

    chore: sending jwt via cookie

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObject = {}
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)){
+      newObject[el] = obj[el]
+    }
+  })
+  return newObject
+}
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find()
@@ -12,6 +23,40 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     }
   });
 });
+
+exports.updateMe = catchAsync( async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password) {
+    return new AppError('This route is not for password updates. Please use /updateMyPassword', 400)
+  }
+  
+  // 3) Filtered out unwanted names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email')
+  
+  // 2) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  })
+  
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  })
+})
+
+exports.deleteMe = catchAsync( async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, {
+    active: false
+  })
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  })
+})
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,8 +10,9 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-
 router.patch('/updateMyPassword', protectMiddleware.protect, authController.updatePassword)
+router.patch('/updateMe', protectMiddleware.protect, userController.updateMe)
+router.patch('/deleteMe', protectMiddleware.protect, userController.deleteMe)
 
 router
   .route('/')
